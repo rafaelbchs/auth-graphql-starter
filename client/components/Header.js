3024575919bf9e20739cc9d28f88a6c297c04281
rafaelbchs <rafaelbchacins@gmail.com>
2022-10-05 14:57:56 -0500

    added login/register

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,13 +2,39 @@ import React from "react";
 import { graphql } from "react-apollo";
 import CurrentUser from "../queries/CurrentUser";
 import query from '../queries/CurrentUser'
+import {Link} from 'react-router'
 
 const Header = (props) => {
-    console.log(props.data)
+
+    function renderButtons(){
+        const {loading, user} = props.data
+        if (loading) {
+            return <div></div>
+        }
+        if (user) {
+            return <div>Logout</div>
+        } else {
+            return (
+                <div>
+                    <li><Link to="/signup">Signup</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </div>
+            )
+        }
+    }
+
+
     return (
-        <div>
-            Header
-        </div>
+        <nav>
+            <div className="nav-wrapper">
+            <Link to="/" className="brand-logo left">
+                Home
+            </Link>
+            <ul className="right">
+                {renderButtons()}
+            </ul>
+            </div>
+        </nav>
     )
 }
 
