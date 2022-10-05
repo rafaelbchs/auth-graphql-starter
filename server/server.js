88990885356ae7088833a9eaf401a35f34d0e669
rafaelbchs <rafaelbchacins@gmail.com>
2022-10-05 09:59:06 -0500

    started project, added type

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://rafaelchs:rPmPCuWHzRZX6AWE@cluster0.4kwgsud.mongodb.net/?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
