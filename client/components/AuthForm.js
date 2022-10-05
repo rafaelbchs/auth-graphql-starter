30b3920cf0f3a7d8181d679baaf19048ef2daecb
rafaelbchs <rafaelbchacins@gmail.com>
2022-10-05 18:15:27 -0500

    added login mutation

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -7,22 +7,28 @@ class AuthForm extends Component {
         super(props);
         this.state =  {email : "", password: ""}
     }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state)
+    };
     
 
     render() {
         return (
         <div className="row">
-        <form className="col s4">
+        <form onSubmit={this.onSubmit.bind(this)} className="col s4">
             <div className="input-field">
-            <label>Email</label>
-            <input 
+            <input
+                placeholder="Email"
                 value={this.state.email}
                 onChange={e=>{this.setState({email: e.target.value})}}
             />
             </div>
             <div className="input-field">
-            <label>Password</label>
-            <input 
+            <input
+                placeholder="Password"
+                type="password" 
                 value={this.state.password}
                 onChange={e=>{this.setState({password: e.target.value})}}
             />
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,13 +1,24 @@
 import React from "react";
 import AuthForm from "./AuthForm";
+import mutation from '../mutations/Login'
+import {graphql} from 'react-apollo';
+import query from '../queries/CurrentUser'
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+
+    function onSubmit({email, password}){
+        props.mutate({
+            variables: {email, password},
+            refetchQueries: [{query}]
+        })
+    }
+
     return (
         <div>
         <h3>Login</h3>
-            <AuthForm />
+            <AuthForm onSubmit={onSubmit}/>
         </div>
     )
 }
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
